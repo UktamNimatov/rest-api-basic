bebb55b60d2db4513fdeb19fb613210a8c05df3b
UktamNimatov <uktam.nimatov@gmail.com>
2022-11-06 21:21:26 +0500

    daily updates

--- service/src/main/java/com/epam/esm/service/impl/GiftCertificateServiceImpl.java
+++ service/src/main/java/com/epam/esm/service/impl/GiftCertificateServiceImpl.java
@@ -12,6 +12,7 @@ import com.epam.esm.service.GiftCertificateService;
 import com.epam.esm.validator.GiftCertificateValidator;
 import com.epam.esm.validator.TagValidator;
 import com.epam.esm.validator.impl.GiftCertificateValidatorImpl;
+import com.epam.esm.validator.impl.TagValidatorImpl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,7 @@ import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -28,6 +30,7 @@ public class GiftCertificateServiceImpl extends AbstractEntityService<GiftCertif
 
     private final GiftCertificateDao<GiftCertificate> giftCertificateDao;
     private final GiftCertificateValidator giftCertificateValidator = new GiftCertificateValidatorImpl();
+    private final TagValidator tagValidator = new TagValidatorImpl();
     private final TagDao<Tag> tagDao;
 
     @Autowired
@@ -41,13 +44,20 @@ public class GiftCertificateServiceImpl extends AbstractEntityService<GiftCertif
     @Override
     @Transactional
     public boolean insert(GiftCertificate giftCertificate) throws InvalidFieldException, DuplicateResourceException, ServiceException {
-        if (!giftCertificateValidator.checkGiftCertificate(giftCertificate))
-            throw new InvalidFieldException(String.valueOf(ConstantMessages.ERROR_CODE_400),
-                    ConstantMessages.INVALID_GIFT_CERTIFICATE + GiftCertificateValidatorImpl.ERROR_LIST.toString());
-        if (doesAlreadyExist(giftCertificate.getName()))
-            throw new DuplicateResourceException(String.valueOf(ConstantMessages.ERROR_CODE_409),
-                    ConstantMessages.EXISTING_GIFT_CERTIFICATE_NAME);
         try {
+            if (!giftCertificateValidator.checkGiftCertificate(giftCertificate))
+                throw new InvalidFieldException(String.valueOf(ConstantMessages.ERROR_CODE_400),
+                        ConstantMessages.INVALID_GIFT_CERTIFICATE + GiftCertificateValidatorImpl.ERROR_LIST.toString());
+            if (giftCertificateDao.findByName(giftCertificate.getName()).isPresent())
+                throw new DuplicateResourceException(String.valueOf(ConstantMessages.ERROR_CODE_409),
+                        ConstantMessages.EXISTING_GIFT_CERTIFICATE_NAME);
+            if (giftCertificate.getTagList() != null) {
+                boolean checkResult = giftCertificate.getTagList()
+                        .stream()
+                        .allMatch(tag -> tagValidator.checkName(tag.getName()));
+                if (!checkResult) throw new InvalidFieldException(String.valueOf(ConstantMessages.ERROR_CODE_400),
+                        ConstantMessages.INVALID_TAG_NAME);
+            }
             return giftCertificateDao.insert(giftCertificate);
         } catch (DaoException exception) {
             logger.error("error in inserting a gift certificate", exception);
@@ -115,20 +125,6 @@ public class GiftCertificateServiceImpl extends AbstractEntityService<GiftCertif
         } catch (DaoException daoException) {
             throw new ServiceException(daoException);
         }
-    }
-
-//    @Override
-//    public List<GiftCertificate> sortByRequirements(List<GiftCertificate> giftCertificatesList, Map<String, String> requirements) {
-//        return giftCertificateDao.sortByRequirements(giftCertificatesList, requirements);
-//    }
-
-    private boolean doesAlreadyExist(String giftCertificateName) {
-        try {
-            return giftCertificateDao.findByName(giftCertificateName).isPresent();
-        } catch (DaoException daoException) {
-            logger.error(daoException);
-        }
-        return false;
     }
 
     private void checkFieldsForUpdate(GiftCertificate giftCertificate) throws InvalidFieldException {
